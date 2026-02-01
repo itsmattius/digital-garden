d5d7ffe4519def7c0a8eb66374f6dedd89851f35
Mehdi Abedi <abedi1667@gmail.com>
2026-02-01 21:16:25 +0330

    update feed.xml

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -4,33 +4,36 @@ import RSS from "rss";
 import siteMetadata, { BASE_URL, defaultAuthor } from "@/lib/metadata";
 
 export async function GET(request: Request) {
+  const publishedPosts = allPosts.filter((post) => post.status === "published");
+  const lastPostDate = publishedPosts.reduce((latest, post) => {
+    const postDate = post.lastUpdatedDate || post.publishedDate;
+    return postDate && new Date(postDate) > new Date(latest) ? new Date(postDate) : latest;
+  }, new Date(0));
   const feed = new RSS({
     title: siteMetadata.title.default,
     description: siteMetadata.description,
     site_url: BASE_URL,
     feed_url: `${BASE_URL}/feed.xml`,
-    copyright: `© 2023 ${defaultAuthor.name}`,
+    copyright: `© 2026 ${defaultAuthor.name}`,
     language: "en-US",
-    pubDate: new Date(),
+    pubDate: lastPostDate,
   });
 
-  allPosts
-    .filter((post) => post.status === "published")
-    .map((post) => {
-      feed.item({
-        title: post.title,
-        guid: `${BASE_URL}/posts/${post.slug}`,
-        url: `${BASE_URL}/posts/${post.slug}`,
-        date: post.lastUpdatedDate as string,
-        description: post.description || "",
-        author: defaultAuthor.name,
-        categories: post?.tags?.map((tag) => tag) || [],
-      });
+  publishedPosts.map((post) => {
+    feed.item({
+      title: post.title,
+      guid: `${BASE_URL}/posts/${post.slug}`,
+      url: `${BASE_URL}/posts/${post.slug}`,
+      date: post.lastUpdatedDate as string,
+      description: post.description || "",
+      author: defaultAuthor.name,
+      categories: post?.tags?.map((tag) => tag) || [],
     });
+  });
 
   return new Response(feed.xml({ indent: true }), {
     headers: {
-      "Content-Type": "application/atom+xml; charset=utf-8",
+      "Content-Type": "application/xml; charset=utf-8",
     },
   });
 }
