16cf504d119ee80eecdb9bfaa848219e26aa04c5
Mehdi Abedi <abedi1667@gmail.com>
2026-04-30 23:12:27 +0400

    fix: change strips the time/millisecond portion in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,11 @@ import { MetadataRoute } from "next";
 import { allPages, allPosts } from "contentlayer/generated";
 
 import { BASE_URL } from "@/lib/metadata";
-import { projects } from "@/lib/projects-data";
+
+function formatDate(date: Date | string | undefined) {
+  if (!date) return undefined;
+  return new Date(date).toISOString().split("T")[0];
+}
 
 function getLastModifiedDate(Posts: typeof allPosts) {
   return Posts.reduce((latest, post) => {
@@ -12,25 +16,24 @@ function getLastModifiedDate(Posts: typeof allPosts) {
 }
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const now = new Date();
   const loadedPosts = allPosts.filter((post) => post.status === "published");
-  const lastPostDate = getLastModifiedDate(loadedPosts);
+  const lastPostDate = formatDate(getLastModifiedDate(loadedPosts));
   const posts = loadedPosts.map((post) => ({
     url: `${BASE_URL}/posts/${post.slug}`,
-    lastModified: post.lastUpdatedDate || post.publishedDate,
+    lastModified: formatDate(post.lastUpdatedDate || post.publishedDate),
   }));
 
   const tagsFromPosts = Array.from(new Set(loadedPosts.map((post) => post.tags || []).flat()));
   const tags = tagsFromPosts.map((tag) => ({
     url: `${BASE_URL}/tags/${tag}`,
-    lastModified: getLastModifiedDate(loadedPosts.filter((post) => post.tags?.includes(tag))),
+    lastModified: formatDate(getLastModifiedDate(loadedPosts.filter((post) => post.tags?.includes(tag)))),
   }));
 
   const pages = allPages
     .filter((page) => page.status === "published")
     .map((page) => ({
       url: `${BASE_URL}/${page.slug.split("/pages")}`,
-      lastModified: page.lastUpdatedDate,
+      lastModified: formatDate(page.lastUpdatedDate),
     }));
   return [
     {
